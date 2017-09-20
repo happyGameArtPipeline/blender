43507174e5c419efa1c7dc6d2073f99a1cc4f523
Porteries Tristan <republicthunderbolt9@gmail.com>
2017-09-20 08:02:34 +0200

    Fix loop tri acces.

--- source/gameengine/Converter/BL_BlenderDataConversion.cpp
+++ source/gameengine/Converter/BL_BlenderDataConversion.cpp
@@ -544,8 +544,6 @@ RAS_MeshObject* BL_ConvertMesh(Mesh *me, Object *blenderobj, KX_Scene *scene, BL
 void BL_ConvertDerivedMeshToArray(DerivedMesh *dm, Mesh *me, const std::vector<BL_MeshMaterial>& mats,
 		const RAS_MeshObject::LayersInfo& layersInfo, std::vector<RAS_Polygon> *polygons)
 {
-	DM_ensure_looptri_data(dm);
-
 	const MVert *mverts = dm->getVertArray(dm);
 	const int totverts = dm->getNumVerts(dm);
 	const MPoly *mpolys = (MPoly *)dm->getPolyArray(dm);
